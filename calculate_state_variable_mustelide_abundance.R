5aea42c00ac6cd383dff585816a1e66873d8a60e
hannaboe <hanna_boehner@gmx.de>
2025-06-11 12:51:26 +0200

    uploaded state variable 2024

--- calculate_state_variable_mustelide_abundance.R
+++ calculate_state_variable_mustelide_abundance.R
@@ -29,6 +29,7 @@ if (!require("remotes")) install.packages("remotes")
 if (!require("ckanr")) remotes::install_github("ropensci/ckanr"); library("ckanr")
 if (!require("tidyverse")) install.packages("tidyverse"); library("tidyverse")
 if (!require("lubridate")) install.packages("tidyverse"); library("lubridate")
+if (!require("arrow")) install.packages("tidyverse"); library("arrow")
 
 
 ## setup the connection to the data portal
@@ -39,6 +40,9 @@ COAT_key <- Sys.getenv("API_coat") # write here your API key if you are a regist
 
 ckanr_setup(url = COAT_url, key = COAT_key) # set up the ckanr-API
 
+## download functions from github
+source("https://github.com/COATnor/data_management_scripts/blob/master/download_parquet_files_from_coat_data_portal.R?raw=TRUE")
+source("https://github.com/COATnor/data_preprocessing_scripts/blob/master/function_preprocessing_image_classifications_small_mamma_camara_traps.R?raw=TRUE")
 
 ## ---------------------------------- ##
 ## DEFINE FUNCTIONS
@@ -98,45 +102,45 @@ download_coat_data <- function(COAT_key = COAT_key,
 ## ---------------------------------- ##
 
 ## define year(s) for which the data should be downloaded
-years_ko <- 2016:2022  # komagdalen lemming blocks
-years_vj <- 2019:2022  # vestre jakobselv lemming blocks
-years_rv <- 2021:2022  # river valleys
+years_ko <- 2016:2024  # komagdalen lemming blocks
+years_vj <- 2019:2024  # vestre jakobselv lemming blocks
+years_rv <- 2021:2024  # river valleys
 
 ## specify filenames
-names_class_lb <- c(paste0("V_rodents_cameratraps_image_classification_lemming_blocks_komagdalen_", years_ko, ".txt"),
-                    paste0("V_rodents_cameratraps_image_classification_lemming_blocks_vestre_jakobselv_", years_vj, ".txt"))
+names_class_lb <- c(paste0("V_rodents_cameratraps_image_classification_lemming_blocks_komagdalen_", years_ko, ".parquet"),
+                    paste0("V_rodents_cameratraps_image_classification_lemming_blocks_vestre_jakobselv_", years_vj, ".parquet"))
 
-names_meta_lb <- c(paste0("V_rodents_cameratraps_image_metadata_lemming_blocks_komagdalen_", years_ko, ".txt"),
-                    paste0("V_rodents_cameratraps_image_metadata_lemming_blocks_vestre_jakobselv_", years_vj, ".txt"))
+names_meta_lb <- c(paste0("V_rodents_cameratraps_image_metadata_lemming_blocks_komagdalen_", years_ko, ".parquet"),
+                    paste0("V_rodents_cameratraps_image_metadata_lemming_blocks_vestre_jakobselv_", years_vj, ".parquet"))
 
-names_class_rv <- c(paste0("V_rodents_cameratraps_image_classification_intensive_quadrats_komagdalen_", years_rv, ".txt"),
-                    paste0("V_rodents_cameratraps_image_classification_intensive_quadrats_vestre_jakobselv_", years_rv, ".txt"))
+names_class_rv <- c(paste0("V_rodents_cameratraps_image_classification_intensive_quadrats_komagdalen_", years_rv, ".parquet"),
+                    paste0("V_rodents_cameratraps_image_classification_intensive_quadrats_vestre_jakobselv_", years_rv, ".parquet"))
 
-names_meta_rv <- c(paste0("V_rodents_cameratraps_image_metadata_intensive_quadrats_komagdalen_", years_rv, ".txt"),
-                   paste0("V_rodents_cameratraps_image_metadata_intensive_quadrats_vestre_jakobselv_", years_rv, ".txt"))
+names_meta_rv <- c(paste0("V_rodents_cameratraps_image_metadata_intensive_quadrats_komagdalen_", years_rv, ".parquet"),
+                   paste0("V_rodents_cameratraps_image_metadata_intensive_quadrats_vestre_jakobselv_", years_rv, ".parquet"))
 
 
 ## download image classification and metadata for lemming blocks
-classification_lb_list <- download_coat_data(COAT_key = Sys.getenv("api_COAT"),
-                                         name = "v_rodents_cameratraps_image_classification_lemming_blocks_v3",
-                                         version = 3, 
+classification_lb_list <- download_coat_parquet(COAT_key = Sys.getenv("api_COAT"),
+                                         name = "v_rodents_cameratraps_image_classification_lemming_blocks_v4",
+                                         version = 4, 
                                          filenames = names_class_lb) 
 
-metadata_lb_list <- download_coat_data(COAT_key = Sys.getenv("api_COAT"),
-                                             name = "v_rodents_cameratraps_image_metadata_lemming_blocks_v3",
-                                             version = 3, 
+metadata_lb_list <- download_coat_parquet(COAT_key = Sys.getenv("api_COAT"),
+                                             name = "v_rodents_cameratraps_image_metadata_lemming_blocks_v4",
+                                             version = 4, 
                                              filenames = names_meta_lb) 
 
 
 ## download image classification and metadata for river valleys
-classification_rv_list <- download_coat_data(COAT_key = Sys.getenv("api_COAT"),
-                                             name = "v_rodents_cameratraps_image_classification_intensive_quadrats_v2",
-                                             version = 2, 
+classification_rv_list <- download_coat_parquet(COAT_key = Sys.getenv("api_COAT"),
+                                             name = "v_rodents_cameratraps_image_classification_intensive_quadrats_v3",
+                                             version = 3, 
                                              filenames = names_class_rv) 
 
-metadata_rv_list <- download_coat_data(COAT_key = Sys.getenv("api_COAT"),
-                                       name = "v_rodents_cameratraps_image_metadata_intensive_quadrats_v2",
-                                       version = 2, 
+metadata_rv_list <- download_coat_parquet(COAT_key = Sys.getenv("api_COAT"),
+                                       name = "v_rodents_cameratraps_image_metadata_intensive_quadrats_v3",
+                                       version = 3, 
                                        filenames = names_meta_rv) 
 
 
@@ -150,8 +154,11 @@ lb_processed <- c()
 
 for (i in 1:length(classification_lb_list)) {
   lb_processed[[i]] <- preprocess_classifications(dat_name = classification_lb_list[[i]], meta_name = metadata_lb_list[[i]], is.dir = FALSE)
-  lb_processed[[i]]$t_year <- sub(".*_(\\d{4})\\.txt$", "\\1", names_class_lb[i])  # add year
+  lb_processed[[i]]$t_year <- sub(".*_(\\d{4})\\.parquet$", "\\1", names_class_lb[i])  # add year
 }
+
+## ko 2024 -> 2 missing images -> all classes NA -> ok
+## vj 2023 -> image with tow animals -> ok
 
 lb_dat <- do.call(rbind, lb_processed)
 
@@ -161,7 +168,7 @@ rv_processed <- c()
 
 for (i in 1:length(classification_rv_list)) {
   rv_processed[[i]] <- preprocess_classifications(dat_name = classification_rv_list[[i]], meta_name = metadata_rv_list[[i]], is.dir = FALSE)
-  rv_processed[[i]]$t_year <- sub(".*_(\\d{4})\\.txt$", "\\1", names_class_rv[i])  # add year
+  rv_processed[[i]]$t_year <- sub(".*_(\\d{4})\\.parquet$", "\\1", names_class_rv[i])  # add year
 }
 
 rv_dat <- do.call(rbind, rv_processed)
@@ -179,6 +186,7 @@ dat_all <- rbind(lb_dat, rv_dat)
 ## ---------------------------------- ##
 
 years <- unique(dat_all$t_year) 
+state_var_names <- c()
 
 for (i in 1:length(years)) {
   
@@ -193,35 +201,25 @@ for (i in 1:length(years)) {
     mutate(t_year_week = factor(t_year_week, levels = unique(t_year_week))) %>% 
     dplyr::group_by(sn_region, sn_locality, sc_type_of_sites_ecological, sn_site, t_year, t_week, t_year_week, v_class_id) %>% 
     dplyr::summarise(v_abundance = sum(v_presence, na.rm = TRUE)) %>% 
-    arrange(sn_site, t_year_week)
+    arrange(sn_site, t_year_week) %>% 
+    ungroup() %>% 
+    select(-t_year_week)
   
   ## keep only mustelids
   dat_mustelid <- dat_week %>%  filter(v_class_id %in% c("mus_erm", "mus_niv"))
-
-  
-  
-  
-  ## inculde t_year_week??
-  ## one file per data year (or calender year)
-  
-  
-  
   
   ## save the file to a temporary directory (necessary for uploading it)
-  #state_var_names[i] <- paste0("state_variable_snomelt_snowbed_", years[i], ".txt")
-  #write.table(snowmelt[[i]], paste(tempdir(), state_var_names[i], sep = "/"), row.names = FALSE, sep = ";")
-  #print(paste("state variable calculated and saved to temporary directory:", state_var_names[i]))
+  state_var_names[i] <- paste0("V37_special_predators_mustelid_abundance_", years[i], ".txt")
+  write.table(dat_mustelid, paste(tempdir(), state_var_names[i], sep = "/"), row.names = FALSE, sep = ";")
+  print(paste("state variable calculated and saved to temporary directory:", state_var_names[i]))
   
   ## 
-  out.dir <- "C:/Users/hanna/Box/COAT/Modules/Small rodent module/state_variable_developement/mustelide_abundance/data"
-  new_name <- paste0("state_variable_mustelid_abundance_", years[i], ".txt")
-  write.table(dat_mustelid, paste(out.dir, new_name, sep = "/"), row.names = FALSE, sep = ";")
+  #out.dir <- "C:/Users/hbo042/Box/COAT/Modules/Small rodent module/state_variable_developement/mustelide_abundance/data"
+  #new_name <- paste0("state_variable_mustelid_abundance_", years[i], ".txt")
+  #write.table(dat_mustelid, paste(out.dir, new_name, sep = "/"), row.names = FALSE, sep = ";")
   
   
 }
-
-
-
 
 
 ## ---------------------------------- ##
@@ -231,16 +229,16 @@ for (i in 1:length(years)) {
 ## you can either create a new version of the state variable or add the data to a already existing state variable (then you can skip this part)
 
 ## serach for your dataset
-state_name <- "c1_timing_of_snowmelt_snowbed_varanger_v1" # write here the name including the version of the state variable you want to add data to
+state_name <- "v37_special_predators_mustelid_abundance_v1" # write here the name including the version of the state variable you want to add data to
 state_version <- "1" # write here the version of the state variable
 
 pkg_state <- package_search(q = list(paste("name:", state_name, sep = "")), fq = list(paste("version:", state_version, sep = "")), include_private = TRUE, include_drafts = TRUE)$results[[1]] # search for the dataset and save the results
 pkg_state$name # check the name
 
 ## modify metadata of the state variable
-name_new <- "new_test_state_variable_v2" # write here the name of the new version (for example change v1 to v2)
+name_new <- "v37_special_predators_mustelid_abundance_v2" # write here the name of the new version (for example change v1 to v2)
 version_new <- "2" # write here the new version
-end_new <- "2021-08-31" # wirte here the (new) end date of the dataset
+end_new <- "2021-08-31" # write here the (new) end date of the dataset
 
 pkg_state$datasets # check with which dataset the current version of the state variable is associated
 datasets_new <- "v_air_temperature_snowbed_v2" # write here the name (inlcuding the version) of the dataset the should be associated with the new version of the state variable
@@ -296,7 +294,7 @@ package_create(
 ## The state variable has to be created on www.data.coat.no
 
 ## serach for your dataset
-state_name <- "c1_timing_of_snowmelt_snowbed_varanger_v1" # write here the name including the version of the state variable you want to add data to
+state_name <- "v37_special_predators_mustelid_abundance_v1" # write here the name including the version of the state variable you want to add data to
 state_version <- "1" # write here the version of the state variable
 
 pkg_state <- package_search(q = list(paste("name:", state_name, sep = "")), fq = list(paste("version:", state_version, sep = "")), include_private = TRUE, include_drafts = TRUE)$results[[1]] # search for the dataset and save the results
